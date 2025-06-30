f667c5fa5cca51ce09c8c5096b49a0214508e2ec
web Ivarscom <web@ivarscomagenciadepublicidad.com>
2025-06-30 11:48:34 +0200

    rename route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\DominioController;
 use App\Http\Controllers\UsersController;
-use App\Livewire\ConsultarDominio;
 use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
@@ -22,9 +22,8 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 
-    Route::get('/dominios/{id}', ConsultarDominio::class)->name('dominios.show');
+    Route::get('/dominio/{id}', [DominioController::class, 'index'])->name('dominios.show');
     Route::get('/users', [UsersController::class, 'index'])->name('users');
-
 });
 
 require __DIR__.'/auth.php';
