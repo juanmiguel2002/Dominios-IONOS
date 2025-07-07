d920c14123413bc466538626c6d0c7c409ef67d8
web Ivarscom <web@ivarscomagenciadepublicidad.com>
2025-07-07 09:14:40 +0200

    renovación correo

--- app/Mail/RenovacionDominio.php
+++ app/Mail/RenovacionDominio.php
@@ -5,8 +5,6 @@ namespace App\Mail;
 use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class RenovacionDominio extends Mailable
@@ -21,22 +19,9 @@ class RenovacionDominio extends Mailable
         $this->dominio = $dominio;
         $this->fecha = Carbon::parse($fecha)->format('d/m/Y');
     }
-
-    public function envelope(): Envelope
+    public function build()
     {
-        return new Envelope(
-            subject: 'Aviso de renovación de dominio: '. $this->dominio,
-        );
-    }
-
-    public function content(): Content
-    {
-        return new Content(
-            view: 'emails.renovacion',
-            with: [
-                'dominio' => $this->dominio,
-                'fecha' => $this->fecha,
-            ]
-        );
+        return $this->subject('Correo desde Livewire')
+                    ->view('emails.renovacion');
     }
 }
