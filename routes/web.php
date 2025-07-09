2264fd486adc414bd2b070dd657f16bc8f425149
web Ivarscom <web@ivarscomagenciadepublicidad.com>
2025-07-09 10:24:57 +0200

    rename route

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
 
     Route::get('/dominio/{id}', [DominioController::class, 'index'])->name('dominios.show');
-    Route::post('/dominio/{id}/enviar-email', [DominioController::class, 'enviarEmail'])->name('dominio.enviarEmail');
+    Route::post('/dominio/{id}/enviar-email', [DominioController::class, 'enviarEmail'])->name('enviar-email');
     Route::get('/users', [UsersController::class, 'index'])->name('users');
 });
 
