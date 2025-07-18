14a42b746c76a86d2e27e5255e0627b2873a45d2
web Ivarscom <web@ivarscomagenciadepublicidad.com>
2025-07-18 10:34:15 +0200

    config command

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
+    ->withCommands([
+        __DIR__.'/app/Console/Commands/EnviarRenovaciones',
+    ])
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
