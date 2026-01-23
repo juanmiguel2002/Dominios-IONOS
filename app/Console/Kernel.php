f561b14be5f1b593acff5b3db71109693f8e1c65
web Ivarscom <web@ivarscomagenciadepublicidad.com>
2026-01-23 10:05:23 +0100

    rename command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('dominios:enviar-renovaciones')->dailyAt('09:00');
+        $schedule->command('renovacion:cron')->dailyAt('09:00');
         // Add your scheduled tasks here.
     }
 
